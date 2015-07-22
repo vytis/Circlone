e32515e78a2ee2c620b5a3c2c4eba63608ba6612
Vytis Šibonis <vytis@wahanda.com>
2015-07-22 18:50:21 +0300

    Increased number of iterations in tests and shared the scheme

--- CircloneTests/CircloneTests.swift
+++ CircloneTests/CircloneTests.swift
@@ -13,7 +13,7 @@ class CircloneTests: XCTestCase {
     func testCollisionPerformance() {
         var circles: [Circle] = []
         srand(100)
-        for _ in 0...10000 {
+        for _ in 0...100000 {
             let circle = Circle(x:Float(rand() % 500), y:Float(rand() % 500), radius:Float(rand() % 50))
             if circle.fits(circles) {
                 circles += [circle]

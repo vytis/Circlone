346a10f374f03e72114a072896fec07eeced9ea4
Vytis Šibonis <vytis@wahanda.com>
2015-08-14 09:43:50 +0300

    Added function that does asyn hatching

--- Circlone/Hatchery.swift
+++ Circlone/Hatchery.swift
@@ -40,6 +40,19 @@ class Hatchery {
         return generator.generate(viewport, maxSize: maxSize)
     }
     
+    private func randomCircles(count: Int) -> [Circle] {
+        var circles: [Circle] = []
+        
+        while circles.count < count {
+            let circle = randomCircle()
+            if (circle.fits(circles)) {
+                circles += [circle]
+            }
+        }
+
+        return circles
+    }
+    
     private func hatch() {
         dispatch_async(hatchQueue) {
             while(self.running) {
@@ -48,6 +61,33 @@ class Hatchery {
                 
                 if (circle.fits(circles)) {
                     self.storage.pushNew(circle)
+                }
+            }
+        }
+    }
+
+    private func asyncHatch() {
+        dispatch_async(hatchQueue) {
+            let q = dispatch_queue_create("com.circlone.hatch.collisions", DISPATCH_QUEUE_CONCURRENT)
+
+            while(self.running) {
+                
+                let count = 1024
+                let newCircles = self.randomCircles(count)
+                
+                let existingCircles = self.storage.fetchAll()
+                
+                let paralellCount = 16
+                let perIteration = count / paralellCount
+                dispatch_apply(paralellCount, q) {
+                    
+                    for idx in 0..<perIteration {
+                        let circle = newCircles[$0 * paralellCount + idx]
+                        
+                        if (circle.fits(existingCircles)) {
+                            self.storage.pushNew(circle)
+                        }
+                    }
                 }
             }
         }

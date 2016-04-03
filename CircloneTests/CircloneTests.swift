2f9268f39ebcaaae0cdfda27e62192b2434c24c4
Vytis Šibonis <vytis@wahanda.com>
2016-04-03 19:59:03 +0300

    Updated performance tests

--- CircloneTests/CircloneTests.swift
+++ CircloneTests/CircloneTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Circlone
 
 class CircloneTests: XCTestCase {
     
@@ -15,7 +16,7 @@ class CircloneTests: XCTestCase {
         srand(100)
         for _ in 0...100000 {
             let circle = Circle(x:Float(rand() % 500), y:Float(rand() % 500), radius:Float(rand() % 50))
-            if circle.fits(circles) {
+            if !circle.collides(circles) {
                 circles += [circle]
             }
         }
@@ -24,23 +25,9 @@ class CircloneTests: XCTestCase {
             var newCircles: [Circle] = []
             for _ in 0...10000 {
                 let circle = Circle(x:Float(rand() % 500), y:Float(rand() % 500), radius:Float(rand() % 50))
-                if circle.fits(circles) {
+                if !circle.collides(circles) {
                     newCircles += [circle]
                 }
-            }
-        }
-    }
-    
-    func testGenerationPerformance() {
-        self.measureBlock() {
-            let viewport = Viewport(height: 500, width: 500)
-            let generator = SeededRandomGenerator(seed: 100)
-            let hatchery = Hatchery(viewport: viewport, maxSize: 50, generator: generator)
-            hatchery.running = true
-
-            var circles: [Circle] = []
-            while circles.count < 500 {
-                circles += hatchery.popNewCircles()
             }
         }
     }

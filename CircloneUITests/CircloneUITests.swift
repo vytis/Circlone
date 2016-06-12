5fa034cf098ea4ebc942031dc35297ee71249fa0
Vytis Šibonis <vytis@wahanda.com>
2016-06-12 21:12:34 +0300

    Added fastlane configuration and screenshot generation

--- CircloneUITests/CircloneUITests.swift
+++ CircloneUITests/CircloneUITests.swift
@@ -9,27 +9,20 @@
 import Foundation
 import XCTest
 
+
 class CircloneUITests: XCTestCase {
-        
+    
+    let app = XCUIApplication()
+
     override func setUp() {
-        super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        setupSnapshot(app)
+        app.launch()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testScreenshots() {
+        snapshot("01Shake")
+        let window = app.windows.elementBoundByIndex(0)
+        window.tap()
+        snapshot("02Circles")
     }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
 }
